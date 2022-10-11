f2dbbd6742ffb237061a2d0d86349edfba829f93
John Duncan <john.a.duncan@utexas.edu>
2022-10-11 17:21:56 -0500

    Transform measurements into tracker frame

--- include/sensor_models.h
+++ include/sensor_models.h
@@ -4,8 +4,11 @@
 #include <Eigen/Dense>
 
 #include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/PoseArray.h>
-#include <tf/transform_listener.h>
+// #include <geometry_msgs/TransformStamped.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 #include "gaussian_datatypes.h"
 #include "trackers.h"
@@ -31,9 +34,9 @@ namespace SensorModels {
             _probDetect(prob),
             _trackerFrame(tracker_frame),
             _obsMatrix(Eigen::MatrixXf::Zero(2,4)),
-            _obsCovMatrix(Eigen::MatrixXf::Identity(2,2))
+            _obsCovMatrix(Eigen::MatrixXf::Identity(2,2)),
+            _listener(_tfBuffer)
             {
-                
                 _obsMatrix.topLeftCorner(2,2) = Eigen::MatrixXf::Identity(2,2);
                 _obsCovMatrix = _obsCovMatrix*pow(variance,2);
             };
@@ -77,6 +80,37 @@ namespace SensorModels {
                 tracker->UpdatePostWeights(_probDetect);
 
                 // TODO reserve additional memory in tracker state for matched targets
+
+                // Iterate through all measurements
+                geometry_msgs::PoseStamped originalPose;
+                originalPose.header = msg->header;
+                for (auto& pose : msg->poses )
+                {
+                    // std::cout << "Got measurement" << pose << std::endl;
+                    originalPose.pose = pose;
+                    geometry_msgs::PoseStamped transformedPose;
+
+                    // Convert sensor measurement into tracker frame TODO: move this to beginning of callback or initial
+                    try {
+                        _tfBuffer.transform(originalPose,transformedPose,_trackerFrame);
+                    }
+                    catch (tf2::TransformException &ex) {
+                        ROS_WARN("%s",ex.what());
+                        ros::Duration(1.0).sleep();
+                        continue;
+                    }
+                    // std::cout << "Transformed" << transformedPose << std::endl;
+
+                    _transformedMeasurement(0) = transformedPose.pose.position.x;
+                    _transformedMeasurement(1) = transformedPose.pose.position.y;
+                    std::cout << "Transformed \n" << _transformedMeasurement << std::endl;
+
+                    // TODO - iterate through each expected state & add to state
+
+                }
+
+                // Match measurements to expected targets
+
             }
 
         private:
@@ -87,13 +121,18 @@ namespace SensorModels {
             Eigen::Matrix<float, 2, 2> _obsCovMatrix;   // Observation covariance matrix
 
             GaussianDataTypes::GaussianMixture<4> _expectedState;       // Expected/previous state
+
             int _nObjects;  // number of objects in state
             std::vector<Eigen::Matrix<float, 2, 1>> _expectedMeas;      // Expected measurement mean
             std::vector<Eigen::Matrix<float, 2, 2>> _innovCovMatrix;    // Innovation covariance matrix
             std::vector<Eigen::Matrix<float, 4, 2>> _kalmanGain;        // Kalman gain matrices
             std::vector<Eigen::Matrix<float, 4, 4>> _postCovMatrix;     // Posterior state covariance matrix
 
-            //tf::TransformListener _listener;    // Transform listener
+            Eigen::Matrix<float, 2, 1> _transformedMeasurement;        // Measurement converted into the tracker frame
+
+            tf2_ros::Buffer _tfBuffer;                  // Transform buffer
+            tf2_ros::TransformListener _listener; // Transform listener
+            geometry_msgs::TransformStamped _transform;            // transform between sensor and tracker frames
             std::string _trackerFrame;
     };
 
